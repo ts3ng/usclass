df23a4762b716dfa78d9f3ce257b459b951b99bc
William Kennedy <bill@ardanlabs.com>
2022-01-31 14:45:08 -0500

    push2

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ardanlabs/service/foundation/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -17,4 +20,21 @@ func main() {
 	}
 	defer log.Sync()
 
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	log.Infow("startup", "hi my logging is working")
+	defer log.Infow("startup", "now I'm shutting down")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	return nil
 }
--- makefile
+++ makefile
@@ -1,5 +1,11 @@
 SHELL := /bin/bash
 
+run:
+	go run app/services/sales-api/main.go | go run app/tooling/logfmt/main.go
+
+down:
+	kill -INT $(shell ps | grep go-build | grep -v grep | sed -n 2,2p | cut -c1-5)
+
 # ==============================================================================
 # Modules support
 
