4ae550a921eed1e5b0dcb88196113f4839249f46
William Kennedy <bill@ardanlabs.com>
2022-02-02 11:56:52 -0500

    day3 push2

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -2,10 +2,31 @@
 package handlers
 
 import (
+	"encoding/json"
 	"expvar"
 	"net/http"
 	"net/http/pprof"
+
+	"github.com/dimfeld/httptreemux/v5"
 )
+
+// APIMux constructs a http.Handler with all application routes defined.
+func APIMux() *httptreemux.ContextMux {
+	mux := httptreemux.NewContextMux()
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		status := struct {
+			Status string
+		}{
+			Status: "OK",
+		}
+		json.NewEncoder(w).Encode(status)
+	}
+
+	mux.Handle(http.MethodGet, "/test", h)
+
+	return mux
+}
 
 // DebugStandardLibraryMux registers all the debug routes from the standard library
 // into a new mux bypassing the use of the DefaultServerMux. Using the
