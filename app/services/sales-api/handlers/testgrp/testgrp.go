3051f7dc8026b1554a9b12c7d91eaba096f946ce
William Kennedy <bill@ardanlabs.com>
2022-02-02 14:57:40 -0500

    day3 push10

--- app/services/sales-api/handlers/testgrp/testgrp.go
+++ app/services/sales-api/handlers/testgrp/testgrp.go
@@ -6,12 +6,14 @@ import (
 	"math/rand"
 	"net/http"
 
+	v1Web "github.com/ardanlabs/service/business/web/v1"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
 func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100) % 2; n == 0 {
-		return errors.New("NON TRUSTED")
+		//return errors.New("NON TRUSTED")
+		return v1Web.NewRequestError(errors.New("we trust this message"), http.StatusBadRequest)
 	}
 
 	status := struct {
