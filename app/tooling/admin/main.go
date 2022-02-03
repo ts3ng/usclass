0bdaaa3734ebe346bb73cc876bb4a08af1fdca31
William Kennedy <bill@ardanlabs.com>
2022-02-03 14:56:59 -0500

    Day4 Push18

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -11,6 +11,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/ardanlabs/service/business/sys/auth"
+	"github.com/ardanlabs/service/foundation/keystore"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 )
@@ -30,6 +32,54 @@ type Claims struct {
 }
 
 func GenToken() error {
+
+	// Construct a key store based on the key files stored in
+	// the specified directory.
+	keysFolder := "zarf/keys/"
+	ks, err := keystore.NewFS(os.DirFS(keysFolder))
+	if err != nil {
+		return fmt.Errorf("reading keys: %w", err)
+	}
+
+	// Init the auth package.
+	activeKID := "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+	a, err := auth.New(activeKID, ks)
+	if err != nil {
+		return fmt.Errorf("constructing auth: %w", err)
+	}
+
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := auth.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "123456789",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	// This will generate a JWT with the claims embedded in them. The database
+	// with need to be configured with the information found in the public key
+	// file to validate these claims. Dgraph does not support key rotate at
+	// this time.
+	token, err := a.GenerateToken(claims)
+	if err != nil {
+		return fmt.Errorf("generating token: %w", err)
+	}
+
+	fmt.Printf("-----BEGIN TOKEN-----\n%s\n-----END TOKEN-----\n", token)
+
+	return nil
+}
+
+func GenTokenHack() error {
 
 	// Generate a new private key.
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
