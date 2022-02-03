e83dcb03b51db93f52cb9cba01c393366f116738
William Kennedy <bill@ardanlabs.com>
2022-02-03 13:11:23 -0500

    Day4 Push11

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -5,17 +5,89 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 func main() {
-	err := GenKey()
+	//err := GenKey()
+	err := GenToken()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+	Roles []string `json:"roles"`
+}
+
+func GenToken() error {
+
+	// Generate a new private key.
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	// Write the public key to the private key file.
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("==============================================================")
+
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   uuid.NewString(),
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"Admin"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	if method == nil {
+		return errors.New("configuring algorithm RS256")
+	}
+
+	token := jwt.NewWithClaims(method, claims)
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return fmt.Errorf("signing token: %w", err)
+	}
+
+	fmt.Println(str)
+
+	return nil
 }
 
 // GenKey creates an x509 private/public key for auth tokens.
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/ardanlabs/conf/v3 v3.1.1
 	github.com/dimfeld/httptreemux/v5 v5.4.0
 	github.com/emadolsky/automaxprocs v1.4.1-0.20220104203543-349281033d17
+	github.com/golang-jwt/jwt/v4 v4.2.0
 	github.com/google/uuid v1.3.0
 	go.uber.org/zap v1.20.0
 )
